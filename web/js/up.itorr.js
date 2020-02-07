033600736d6e638cabffa858cda6bfb73d506974
EngiGu <sayheya@qq.com>
2020-02-07 20:26:13 +0800

    update

--- web/js/up.itorr.js
+++ web/js/up.itorr.js
@@ -2,33 +2,33 @@ var staticPath = '';
 var apiUrl = 'https://apis.yum6.cn/api/5bd44dc94bcfc';
 var YoungxjApisToken = 'f07b711396f9a05bc7129c4507fb65c5';
 
-UP=function(o,success,error,upload,x,file,A){
+UP = function (o, success, error, upload, x, file, A) {
 	// $.ajaxSettings.async = false; 
 	// $.get('http://mouto.org/api/hosts',function(r){
 	// var ip=''+r.cmcc.match(/[\d\.]+/);
 	// staticPath='http://'+ip+':'+'672/';
 	// alert(staticPath);
-// });
-	if(typeof success=='function'){
-		var file = new FormData(); 
+	// });
+	if (typeof success == 'function') {
+		var file = new FormData();
 		file.append("file", o);
 		// file=o;
 	}
-	else{
-		if(!o.file)
+	else {
+		if (!o.file)
 			return console.log('并没有传入需要上传的文件')
 
-		if(A=o.success)
-			success=A
+		if (A = o.success)
+			success = A
 
-		if(A=o.upload)
-			upload=A
-		
-		if(A=o.error)
-			error=A
+		if (A = o.upload)
+			upload = A
+
+		if (A = o.error)
+			error = A
 	}
 
-	x=new XMLHttpRequest()
+	x = new XMLHttpRequest();
 	// alert(staticPath+'v1/upload');
 	// x.open('POST',apiUrl+'?token='+YoungxjApisToken , false);
 	// url = '/api/upload'
@@ -36,23 +36,16 @@ UP=function(o,success,error,upload,x,file,A){
 	// x.open('POST','http://114.86.196.54:672/v1/upload',true);
 	x.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
-	if(upload)
-		x.upload.onprogress=function(e){
-			upload(e.loaded/e.total)
+	if (upload) {
+		x.upload.onprogress = function (e) {
+			upload(e.loaded / e.total)
 		}
-
-	x.onload=function(r){
-		r=JSON.parse(x.responseText)
-		// alert(r.wbpid);
-		// alert(r.error);
-		if(r.error&&error)
-			return error(r.error)
-
-		if(r.data.pid&&success)
-			// alert(r.data.pid);
-			return success(r.data.pid)
+	}
+	x.onload = function (r) {
+		r = JSON.parse(x.responseText);
+		return success(r.url)
 	}
 	x.send(file);
 	// $.ajaxSettings.async = true; 
-	
+
 }
